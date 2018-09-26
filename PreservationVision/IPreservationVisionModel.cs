8be34db9cc7b3d326cd5350f9496edfdfea2610a
Volkov Alexandr <al-tracer-93@mail.ru>
2018-09-26 10:14:01 +0300

    1

---
+++ PreservationVision/IPreservationVisionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PreservationVision
+{
+    public interface IPreservationVisionModel
+    {
+        int TimeRelaxSeconds { get; set; }
+        int IntervalRelaxMinutes { get; set; }
+        DateTime LastWorkTime { get; set; }
+        DateTime LastRelaxTime { get; set; }
+        string TimeUntilEvent { get; }
+        bool IsVisibleWindow { get; set; }
+        bool IsAutoRun { get; set; }
+    }
+}
---
+++ PreservationVision/Parameters.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PreservationVision
+{
+    /// <summary>
+    /// Interaction logic for Parameters.xaml
+    /// </summary>
+    public partial class Parameters : Window
+    {
+        public IPreservationVisionModel Model { get; set; }
+        public Parameters(IPreservationVisionModel model)
+        {
+            Model = model;
+            InitializeComponent();
+            
+        }
+
+        private void window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Visibility = Visibility.Hidden;
+            e.Cancel = true;
+        }
+    }
+}
